a0576b2c931b0483af7f1eb4267035ef1b3d7ef9
Corey Rome <coreyrome2@gmail.com>
2018-01-12 09:17:29 -0600

    added M V C's for player and coach

---
+++ TeamManager/TeamManager/Models/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TeamManager.Models
+{
+    public class Player
+    {
+        [Key]
+        public int playerId { get; set; }
+        public double amountOwed { get; set; }
+        public bool divisionD2 { get; set; }
+        public bool pro { get; set; }
+
+    }
+}
