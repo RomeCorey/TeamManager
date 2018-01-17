0e86976256ce0904e06091642820854491e57679
Chez <chez.vang@gmail.com>
2018-01-17 10:59:30 -0600

    syncing project

--- TeamManager/TeamManager/Controllers/CoachPaymentsController.cs
+++ TeamManager/TeamManager/Controllers/CoachPaymentsController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Stripe;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
@@ -124,6 +125,34 @@ namespace TeamManager.Controllers
                 db.Dispose();
             }
             base.Dispose(disposing);
+        }
+
+        //Stripe API Things
+        public ActionResult Charge(string stripeEmail, string stripeToken)
+        {
+            var customers = new StripeCustomerService();
+            var charges = new StripeChargeService();
+
+            var customer = customers.Create(new StripeCustomerCreateOptions
+            {
+                Email = stripeEmail,
+                SourceToken = stripeToken
+            });
+
+            var charge = charges.Create(new StripeChargeCreateOptions
+            {
+                Amount = 500,
+                Description = "Sample Charge",
+                Currency = "usd",
+                CustomerId = customer.Id
+            });
+
+            return View();
+        }
+
+        public ActionResult Error()
+        {
+            return View();
         }
     }
 }
